4cae9ba465f7d466d3909e5134c44e585031826f
milonhd <107748895+milonhd@users.noreply.github.com>
2025-05-21 23:25:03 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+from pydantic import BaseSettings
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config(BaseSettings):
+    api_token: str
+    database_url: str
+    admin_id: int
+    receipt_dir: str = "/app/receipts"
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+    def __init__(self):
+        super().__init__()
+        if not self.api_token or not self.database_url or not self.admin_id:
+            raise ValueError("Не заданы обязательные переменные окружения: API_TOKEN, DATABASE_URL, ADMIN_ID")
