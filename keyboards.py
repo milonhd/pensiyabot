375ce68b5f82a37a10f3fbf067693412af9c9702
milonhd <107748895+milonhd@users.noreply.github.com>
2025-05-23 16:29:55 +0500

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,25 +1,26 @@
+# keyboards.py
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
-main_keyboard = InlineKeyboardMarkup(inline_keyboard=[ #
-    [InlineKeyboardButton(text="Уровень САМОСТОЯТЕЛЬНЫЙ", callback_data="self")], #
-    [InlineKeyboardButton(text="Уровень БАЗОВЫЙ", callback_data="basic")], #
-    [InlineKeyboardButton(text="Уровень ПРО", callback_data="pro")], #
+main_keyboard = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="Уровень САМОСТОЯТЕЛЬНЫЙ", callback_data="self")],
+    [InlineKeyboardButton(text="Уровень БАЗОВЫЙ", callback_data="basic")],
+    [InlineKeyboardButton(text="Уровень ПРО", callback_data="pro")],
 ])
 
-materials_keyboard = InlineKeyboardMarkup(inline_keyboard=[ #
-    [InlineKeyboardButton(text="🏰 Получить материалы", callback_data="get_materials")] #
+materials_keyboard = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="🏰 Получить материалы", callback_data="get_materials")]
 ])
 
-def get_self_years_keyboard(): #
-    return InlineKeyboardMarkup(inline_keyboard=[ #
-        [InlineKeyboardButton(text=f"Пенсия {year}", callback_data=f"year_{year}")] for year in range(2025, 2032) #
+def get_self_years_keyboard():
+    return InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text=f"Пенсия {year}", callback_data=f"year_{year}")] for year in range(2025, 2032)
     ])
 
-def get_year_buttons(year): #
-    return InlineKeyboardMarkup(inline_keyboard=[ #
-        [InlineKeyboardButton(text="✅ Оплатить", url="https://pay.kaspi.kz/pay/vx2s6z0c")], #
-        [InlineKeyboardButton(text="📄 Отправить чек", callback_data=f"send_screenshot_{year}")] #
+def get_year_buttons(year):
+    return InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="✅ Оплатить", url="https://pay.kaspi.kz/pay/vx2s6z0c")],
+        [InlineKeyboardButton(text="📄 Отправить чек", callback_data=f"send_screenshot_{year}")]
     ])
 
 def get_main_menu_keyboard(admin_id, user_id):
