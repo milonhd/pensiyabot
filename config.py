de3ecbba856fd9bebd9db87e2bc66d49679f0895
milonhd <107748895+milonhd@users.noreply.github.com>
2025-05-23 16:29:21 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,23 @@
 import os
+from dotenv import load_dotenv
 
-API_TOKEN = '7940234323:AAG0GVXl_k4oLefRsZnte-S8PYUvowv2gVU'  #
-ADMIN_ID = 1640165074  #
-GROUP_IDS = [-1002583988789, -1002529607781, -1002611068580, -1002607289832, -1002560662894, -1002645685285, -1002529375771, -1002262602915]  #
-RECEIPT_DIR = "/app/receipts"  #
+# Загружаем переменные из .env файла
+load_dotenv()
 
-# Конфигурация для PostgreSQL
-DATABASE_URL = os.environ.get('DATABASE_URL')  #
-if not DATABASE_URL: #
-    # Локальная конфигурация для тестирования
-    DATABASE_URL = "postgres://username:password@localhost:5432/telegrambot" #
+API_TOKEN = os.getenv('API_TOKEN')
+ADMIN_ID = int(os.getenv('ADMIN_ID'))
+# Разделяем строку GROUP_IDS по запятой и преобразуем в список целых чисел
+GROUP_IDS = [int(g_id) for g_id in os.getenv('GROUP_IDS').split(',')]
+RECEIPT_DIR = os.getenv('RECEIPT_DIR', '/app/receipts') # Добавлено значение по умолчанию
+DATABASE_URL = os.getenv('DATABASE_URL')
 
-os.makedirs(RECEIPT_DIR, exist_ok=True) #
+if not API_TOKEN:
+    raise ValueError("API_TOKEN не установлен в .env")
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL не установлен в .env")
+if not ADMIN_ID:
+    raise ValueError("ADMIN_ID не установлен в .env")
+if not GROUP_IDS:
+    raise ValueError("GROUP_IDS не установлен в .env")
+
+os.makedirs(RECEIPT_DIR, exist_ok=True)
