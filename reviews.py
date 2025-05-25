1bd10760e6041ba3db2ac978146328e945d51429
w1mannd <107748895+w1mannd@users.noreply.github.com>
2025-05-25 17:23:44 +0500

    Update reviews.py

--- reviews.py
+++ reviews.py
@@ -13,7 +13,7 @@ REVIEWS_CHANNEL_ID = -1002513508156
 ADMIN_ID = 957724800
 MIN_REVIEW_INTERVAL = timedelta(minutes=5)
 
-def register_reviews_handlers(dp, bot):
+def register_reviews_handlers(dp, bot, pool):
     global db_pool
     db_pool = pool
 
